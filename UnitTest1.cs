b09a0e9f73acf40fa841651289033a7252e30ba6
thomastl93 <thomastl1993@gmail.com>
2025-07-23 10:34:14 +0100

    Driver Setup and Navigate to URL

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,15 +1,29 @@
-﻿namespace admlucid;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace admlucid;
 
 public class Tests
 {
+    IWebDriver driver;
     [SetUp]
     public void Setup()
     {
+        driver = new ChromeDriver();
+        driver.Manage().Window.Maximize();
     }
 
     [Test]
     public void Test1()
     {
-        Assert.Pass();
+        driver.Navigate().GoToUrl("https://admlucid.com"); //navigating to URL
+        Assert.That(driver.Title, Is.EqualTo("Home Page - Admlucid"));
+        //Assert.Pass();
+    }
+    
+    [TearDown]
+    public void TearDown()
+    {
+        driver.Close();
     }
 }
