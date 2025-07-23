87d1f7c2e1c62706a32f23d078de4995948c26bb
thomastl93 <thomastl1993@gmail.com>
2025-07-23 15:03:11 +0100

    Test updates

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -14,7 +14,7 @@ public class Tests
     public void Setup()
     {
         ChromeOptions options = new ChromeOptions();
-        //options.AddArguments("--headless");
+        options.AddArguments("--headless");
         options.AddArgument("--incognito");
         options.AddArgument("--start-maximized");
         driver = new ChromeDriver(options); //Chrome driver
@@ -61,6 +61,33 @@ public class Tests
         var fileInputId = driver.FindElement(By.Id("Text1"));
         fileInputId.SendKeys(@"C:\\Users\\admlucid\\Downloads\\admlucid.png");
         Assert.That(fileInputId.GetAttribute("value"), Is.EqualTo(@"C:\\Users\\admlucid\\Downloads\\admlucid.png")); //Assertion to validate file path is entered correctly
+    }
+
+    [Test]
+    public void CheckBox()
+    {
+        driver.Navigate().GoToUrl("https://admlucid.com/Home/WebElements");
+        Assert.That(driver.Title, Is.EqualTo("- Admlucid"));
+        var checkboxId = driver.FindElement(By.Id("Checkbox1"));
+        checkboxId.Click();
+        Assert.That(checkboxId.GetAttribute("checked"), Is.EqualTo("true")); //Assertion to validate checkbox is checked
+    }
+
+    [Test]
+    public void FormSubmition()
+    {
+        driver.Navigate().GoToUrl("https://admlucid.com/Home/WebElements");
+        Assert.That(driver.Title, Is.EqualTo("- Admlucid"));
+        
+        var parentsName = driver.FindElement(By.Name("Name"));
+        parentsName.SendKeys("Thomas Tams-Law");
+        Assert.That(parentsName.GetAttribute("value"), Is.EqualTo("Thomas Tams-Law"));
+        
+        var parentsEmail = driver.FindElement(By.Name("EMail"));
+        parentsEmail.SendKeys("test@test.com");
+        Assert.That(parentsEmail.GetAttribute("value"), Is.EqualTo("test@test.com"));
+        
+        
     }
 
     [TearDown]
